d23df72301c47079ccaa86a4dbe7a0fdac3f89f6
Lucas Alvares Gomes <lucasagomes@gmail.com>
2024-12-16 15:59:18 +0000

    Run the iLab sanity check script with Ansible
    Signed-off-by: Lucas Alvares Gomes <lucasagomes@gmail.com>

--- rhelai-validation/scripts/ilab_sanity_check.py
+++ rhelai-validation/scripts/ilab_sanity_check.py
@@ -1,16 +1,8 @@
-import json
-import os
 import sys
 
 import torch
 import torch.nn as nn
 
-RESULT_PATH = os.path.join(os.getcwd(), "ilab_sanity_result.json")
-
-_DATA = {
-    "success": False,
-    "extra": {},
-}
 
 def sanity_check():
     # Define a simple model
@@ -22,18 +14,10 @@ def sanity_check():
     # Print input and output shapes
     print(f"Input shape: {dummy_input.shape}, Output shape: {output.shape}")
 
+SUCCESS = False
 try:
-    _DATA["extra"]["is_cuda_available"] = torch.cuda.is_available()
-    _DATA["extra"]["cude_device_count"] = torch.cuda.device_count()
     sanity_check()
     SUCCESS = True
-except Exception:
-    SUCESS = False
-
-try:
-    _DATA["success"] = SUCCESS
-    with open(RESULT_PATH, "w") as json_file:
-        json.dump(_DATA, json_file, indent=4)
 finally:
     rc = 0 if SUCCESS else 1
     sys.exit(rc)
--- rhelai-validation/scripts/run_ilab_sanity_check.sh
+++ rhelai-validation/scripts/run_ilab_sanity_check.sh
@@ -3,6 +3,6 @@
 # Run the ilab_sanity_check.py script inside the iLab shell
 ILAB_ADDITIONAL_MOUNTS="/tmp/scripts:/scripts" ilab shell << EOF
 python /scripts/ilab_sanity_check.py
-exit
+exit \$?
 EOF
 
