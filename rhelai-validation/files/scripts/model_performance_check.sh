ff7aa89751492aabc2c355def54a514196e9d606
Chris Sibbitt <csibbitt@redhat.com>
2025-09-17 15:56:58 -0400

    Moving away from install_yamls + rhelai...
    ...and towards centos + vllm.
    
    This repo will soon be renamed to gpu-validation with all traces of rhelai removed.
    
    * Moved OSP setup code from old install_yamls nested_passthrough scripts
      * Image and flavor
      * Networking and security group
    * Use cifmw SSH keys by default
    * Uses Centos 9 image instead of RHELAI
    * Tinyllama instead of granite
    * vllm instead of ilab
    * Installs nvidia, cuda, torch, and numpy
    * Fix for spurious DNS problem
    * Fix for metrics changes
    * Removed nginx

--- rhelai-validation/files/scripts/model_performance_check.sh
+++ rhelai-validation/files/scripts/model_performance_check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-MODEL_PATH="${MODEL_PATH:-/var/home/cloud-user/.cache/instructlab/models/granite-8b-lab-v1}"
+MODEL_NAME="${MODEL_NAME:-TinyLlama/TinyLlama-1.1B-Chat-v1.0}"
 
 function do_curl {
     if [ "$1" == "chat" ]; then
         CURL_OUTPUT=$(curl -s --show-error -f -X POST "http://localhost:8000/v1/chat/completions" \
             -H "Content-Type: application/json" \
-            --data '{"model": "'"${MODEL_PATH}"'","messages": [{"role": "user","content": "Who am I speaking to?"}]}')
+            --data '{"model": "'"${MODEL_NAME}"'","messages": [{"role": "user","content": "Who am I speaking to?"}]}')
     elif [ "$1" == "metrics" ]; then
         CURL_OUTPUT=$(curl -s --show-error -f localhost:8000/metrics)
     fi
@@ -23,11 +23,11 @@ done
 
 do_curl "metrics"
 
-TIME_PER_TOK_SUM=$(echo "${CURL_OUTPUT}" | grep time_per_output_token_seconds_sum\{ | cut -d ' ' -f 2)
-TIME_PER_TOK_COUNT=$(echo "${CURL_OUTPUT}" | grep time_per_output_token_seconds_count\{ | cut -d ' ' -f 2)
+TIME_PER_TOK_SUM=$(echo "${CURL_OUTPUT}" | grep vllm:time_per_output_token_seconds_sum\{ | cut -d ' ' -f 2)
+TIME_PER_TOK_COUNT=$(echo "${CURL_OUTPUT}" | grep vllm:time_per_output_token_seconds_count\{ | cut -d ' ' -f 2)
 
-TIME_FIRST_TOK_SUM=$(echo "${CURL_OUTPUT}" | grep time_to_first_token_seconds_sum\{ | cut -d ' ' -f 2)
-TIME_FIRST_TOK_COUNT=$(echo "${CURL_OUTPUT}" | grep time_to_first_token_seconds_count\{ | cut -d ' ' -f 2)
+TIME_FIRST_TOK_SUM=$(echo "${CURL_OUTPUT}" | grep vllm:time_to_first_token_seconds_sum\{ | cut -d ' ' -f 2)
+TIME_FIRST_TOK_COUNT=$(echo "${CURL_OUTPUT}" | grep vllm:time_to_first_token_seconds_count\{ | cut -d ' ' -f 2)
 
 AVG_TIME_PER_TOK=$(python -c "print(round($TIME_PER_TOK_SUM / $TIME_PER_TOK_COUNT, 4))")
 AVG_TIME_TO_FIRST_TOK=$(python -c "print(round($TIME_FIRST_TOK_SUM / $TIME_FIRST_TOK_COUNT, 4))")
